67ab8f0fd99acdac3ebbb5095e10cb7d3da72fe0
olena-semenenko <elen.kinakh@gmail.com>
2023-01-15 22:46:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 var debounce = require('lodash.debounce');
 
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
