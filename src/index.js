0cf0259c73b65a3b578eac387aff689680f8f964
olena-semenenko <elen.kinakh@gmail.com>
2023-01-15 22:26:52 +0100

    Updated _hero

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//import plagins
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const axios = require('axios');
+
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
